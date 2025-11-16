fdd667eb8e833880261c8e9ac2b11d7c2fcfecf6
loihnt05 <23520869@gm.uit.edu.vn>
2025-11-16 11:39:09 +0700

    redis config with cache manager

--- backend/src/cache/redis/redis.controller.ts
+++ backend/src/cache/redis/redis.controller.ts
@@ -1,5 +1,5 @@
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, Post } from '@nestjs/common';
 import type { Cache } from 'cache-manager';
 
 @Controller('redis')
@@ -18,10 +18,31 @@ export class RedisController {
     }
 
     const randomNum = Math.floor(Math.random() * 1000);
-    await this.cacheManager.set('number', randomNum, 1000);
+    await this.cacheManager.set('number', randomNum, 10000);
     return {
       data: randomNum,
       FromRedis: 'This is loaded from randomNumDbs',
     };
+  }
+
+  @Post('clear-number-cache')
+  async clearNumberCache(): Promise<{ message: string }> {
+    await this.cacheManager.del('number');
+    return {
+      message: 'Number cache cleared successfully',
+    };
+  }
+
+  @Get('all-keys')
+  async getAllKeys(): Promise<string> {
+    for (const store of this.cacheManager.stores) {
+      const storeIterator = store?.iterator;
+      if (storeIterator) {
+        for await (const [key, value] of storeIterator('namespace')) {
+          console.log(key, value);
+        }
+      }
+    }
+    return 'See console for all keys';
   }
 }
--- backend/src/cache/redis/redis.module.ts
+++ backend/src/cache/redis/redis.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
-import { redisStore } from 'cache-manager-redis-store';
-import { CacheModule } from '@nestjs/cache-manager/dist/cache.module';
+import { redisStore } from 'cache-manager-redis-yet';
 import { RedisController } from './redis.controller';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
-    CacheModule.register({ store: redisStore, host: 'localhost', port: 6379 }),
+    CacheModule.register({
+      useFactory: () => ({
+        store: redisStore,
+        host: 'localhost',
+        port: 6379,
+        ttl: 60000,
+      }),
+    }),
   ],
   controllers: [RedisController],
   providers: [RedisService],
